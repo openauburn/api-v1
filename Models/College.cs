4b26ab825c0e5f719f76c56ab6829b0b90d30c15
Matthew Rogers <rogersmatt350@gmail.com>
2022-09-10 22:24:33 -0500

    Added API support for College dataset

---
+++ Models/College.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace open_auburn_api.Models
+{
+    [Table("College")]
+    public partial class College
+    {
+        [Key]
+        [Column("ID")]
+        public int Id { get; set; }
+        [Column("OAID")]
+        [StringLength(50)]
+        [Unicode(false)]
+        public string? Oaid { get; set; }
+        public string? Name { get; set; }
+
+        public string? Description { get; set; }
+
+        public string? Website { get; set; }
+
+        public string? Bulletin { get; set; }
+
+        public string? Phone { get; set; }
+
+    }
+}
