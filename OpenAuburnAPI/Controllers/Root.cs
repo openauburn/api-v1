028ed8700c1788e51e5241f1916dabaed3ee5d36
Matthew Rogers <rogersmatt350@gmail.com>
2022-12-09 00:16:41 -0600

    Removed root controller from swagger docs

---
+++ OpenAuburnAPI/Controllers/Root.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using open_auburn_api.Models;
+using open_auburn_api.Filters;
+using open_auburn_api.Helpers;
+using open_auburn_api.Services;
+using open_auburn_api.Wrappers;
+
+#nullable enable
+namespace open_auburn_api.Controllers
+{
+    [Route("/")]
+    [ApiExplorerSettings(IgnoreApi = true)]
+    [ApiController]
+    public class Root : ControllerBase
+    {
+        private readonly OpenAuburnContext _context;
+        private readonly IURIService _uriService;
+
+        public Root(OpenAuburnContext context, IURIService uriService)
+        {
+            _context = context;
+            _uriService = uriService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetDatasets([FromQuery] PaginationFilter pfilter)
+        {
+            var route = Request.Path.Value;
+            var query = PaginationHelper.IsolateFilterQuery(Request.QueryString.ToString());
+
+            var validPFilter = new PaginationFilter(pfilter.PageNumber, pfilter.PageSize);
+            var allData = await _context.Datasets.OrderByDescending(c => c.Id).ToListAsync();
+            var totalRecords = allData.Count;
+            var pagedData = allData
+                .Skip((validPFilter.PageNumber - 1) * validPFilter.PageSize)
+                .Take(validPFilter.PageSize)
+                .ToList();
+            var pagedReponse = PaginationHelper.CreatePagedReponse<Dataset>(pagedData, validPFilter, totalRecords, _uriService, route, query);
+            return Ok(pagedReponse);
+        }
+
+        [HttpGet("favicon.ico")]
+        public IActionResult GetFavicon()
+        {
+            byte[] b = System.IO.File.ReadAllBytes(@".\favicon.ico");
+            Response.ContentType = "image/x-icon";
+            return File(b, "image/x-icon");
+        }
+
+        [HttpGet("Error")]
+        public IActionResult GetError() =>
+            Problem("Something went wrong.");
+    }
+}
